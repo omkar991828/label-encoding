ce202c798ea81850f1d74b88ada830c5041f5c58
Omkar Yadav <omkar991828@gmail.com>
2026-06-01 16:40:47 +0530

    Initial commit

---
+++ label_encoding.py
@@ -0,0 +1,21 @@
+# from sklearn.preprocessing import LabelEncoder
+#
+# colors = ['Red', 'Blue', 'Green', 'Red', 'Blue']
+#
+# le = LabelEncoder()
+#
+# encoded = le.fit_transform(colors)
+#
+# print("Original:", colors)
+# print("Encoded :", encoded)
+
+
+
+
+from sklearn.preprocessing import LabelEncoder
+car=['laborgini','maruti','suzuki','toyota','tavera','rohan','aman','aman','anil']
+le=LabelEncoder()
+encoded=le.fit_transform(car)
+print('original:',car)
+print('encoded',encoded)
+
