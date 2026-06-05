96f47abfe7572d95e8acfbe981a35a231020ad62
Omkar Yadav <omkar991828@gmail.com>
2026-06-05 13:02:22 +0530

    Added project files

--- label_encoding.py
+++ label_encoding.py
@@ -12,11 +12,11 @@
 
 
 
-from sklearn.preprocessing import LabelEncoder
-car=['laborgini','maruti','suzuki','toyota','tavera','rohan','aman','aman','anil','ananya']
-le=LabelEncoder()
-encoded=le.fit_transform(car)
-print('original:',car)
-print('encoded',encoded)
+# from sklearn.preprocessing import LabelEncoder
+# car=['laborgini','maruti','suzuki','toyota','tavera','rohan','aman','aman','anil','ananya']
+# le=LabelEncoder()
+# encoded=le.fit_transform(car)
+# print('original:',car)
+# print('encoded',encoded)
 
 
---
+++ ordinary_encoding.py
@@ -0,0 +1,16 @@
+'''ordinary encoding--
+                     it a technique where categories are mapped to numeric value
+                     in meaningful order or ranking it is encode the value based on custom order
+                     / user defined order'''
+
+# from sklearn.preprocessing import OrdinalEncoder
+#
+# salary=[['low'],
+#         ['midium']
+#     ,['high']
+#     ,['low']
+#     ,['middium']]
+# oe=OrdinalEncoder()
+# encoded=oe.fit_transform(salary)
+# print('original:',salary)
+# print('encoded:',encoded)
